b3db5146299679bd871e6d6eb1b85610b3d2b717
Dinastic <mmantiog@hotmail.gr>
2023-06-11 13:36:56 +0300

    Add project files.

---
+++ E-Learning System 2.0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_Learning_System_2._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
